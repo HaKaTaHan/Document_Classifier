eb559d351bdd81bf5d3b57534f770e8f7790c9de
CrewDaniel <alaskay@naver.com>
2020-04-07 19:08:14 +0900

    minor fix

--- GUImain.py
+++ GUImain.py
@@ -15,7 +15,8 @@ from stepListener import interfaceStepListener as stepListener
 # Index 2: showCover
 # Index 3: noCover
 # Index 4: choice
-# Index 5: finish
+# Index 5: inputKeyword
+# Index 6: finish
 
 timer = time.time()
 
